9baa78c763414c42754e41d6698c78e4c02eb409
MatheusANDLucca <matheusconts00@gmail.com>
2024-01-07 11:55:12 -0300

    aula 24 e 25

---
+++ Modulo1/aula011/index.js
@@ -0,0 +1,32 @@
+//               01234567
+let umaString = 'Um texto';
+let umaString2 = 'o \rrato'
+let umaString1 = 'o rato roeu a roupa do rei de roma.';
+console.log(umaString[4]);
+console.log(umaString.concat(' em um lindo dia'))
+
+console.log(umaString2);
+console.log(umaString.indexOf('texto'));
+console.log(umaString.lastIndexOf('m', 3));
+console.log(umaString.match(/[a-z]/g));
+console.log(umaString.search(/[a-z]/));
+console.log(umaString.replace('Um', 'outro'));
+
+console.log(umaString1.length);
+console.log(umaString1.slice(2, 7));
+console.log(umaString1.slice(-5, -1));
+
+console.log(umaString1.split(' ', 3));
+
+console.log(umaString1.toUpperCase());
+console.log(umaString1.toLowerCase());
+
+
+
+
+
+
+
+
+
+
